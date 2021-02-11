7c372f5b03d8a037aa9aa78a69f597c962607251
Syed Zubair Ahmed <syedzubairahmed001@gmail.com>
2021-02-11 19:22:22 +0530

    adds product hunt embed

--- containers/Home/Home.container.jsx
+++ containers/Home/Home.container.jsx
@@ -14,15 +14,24 @@ const Home = (props) => {
               <span className={styles.gradientBg}>Browser!</span>
             </h1>
             <h2 className={styles.subTitle}>
-              Share and watch stories from people around the world while you browse
+              Share and watch stories from people around the world while you
+              browse
             </h2>
             <div className={styles.downloadWrapper}>
               <p className={styles.downloadHeading}>Get it for:</p>
               <div className={styles.downloadSection}>
-                <a href="https://chrome.google.com/webstore/detail/stories/cpflpimmgoikimlpdpihhecigmgehagn/related?hl=en-GB" target="_blank" className={styles.downloadLink}>
+                <a
+                  href="https://chrome.google.com/webstore/detail/stories/cpflpimmgoikimlpdpihhecigmgehagn/related?hl=en-GB"
+                  target="_blank"
+                  className={styles.downloadLink}
+                >
                   <img src="/assets/images/chrome_logo.png" alt="screenshot" />
                 </a>
-                <a href="https://microsoftedge.microsoft.com/addons/detail/stories/dmfflnlaaalkihcikmmbdacijlfjkklc" target="_blank" className={styles.downloadLink}>
+                <a
+                  href="https://microsoftedge.microsoft.com/addons/detail/stories/dmfflnlaaalkihcikmmbdacijlfjkklc"
+                  target="_blank"
+                  className={styles.downloadLink}
+                >
                   <img src="/assets/images/edge_logo.png" alt="screenshot" />
                 </a>
               </div>
@@ -30,8 +39,22 @@ const Home = (props) => {
             <div>
               <p className={styles.para1}>
                 Created with ❤️ by{" "}
-                <a href="https://twitter.com/_syed_zubair" target="_blank">Syed Zubair Ahmed</a>
+                <a href="https://twitter.com/_syed_zubair" target="_blank">
+                  Syed Zubair Ahmed
+                </a>
               </p>
+              <a
+                href="https://www.producthunt.com/posts/stories-in-browser?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-stories-in-browser"
+                target="_blank"
+              >
+                <img
+                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=284261&theme=light"
+                  alt="Stories in browser - Share and watch stories while you browse | Product Hunt"
+                  style={{width: "250px", height: "54px"}}
+                  width="250"
+                  height="54"
+                />
+              </a>
             </div>
           </div>
         </div>
