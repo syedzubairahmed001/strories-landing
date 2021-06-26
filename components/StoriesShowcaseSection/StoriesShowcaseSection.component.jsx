591e211dcae34d716b955e6a002aa407b2cef488
Syed Zubair Ahmed <syedzubairahmed001@gmail.com>
2021-06-26 18:01:36 +0530

    adds text size fix in stories section

--- components/StoriesShowcaseSection/StoriesShowcaseSection.component.jsx
+++ components/StoriesShowcaseSection/StoriesShowcaseSection.component.jsx
@@ -7,7 +7,7 @@ const StoriesShowcase = (props) => {
   return (
     <div className={`${styles.container} bg-light-1 flex-column-mobile mb-40`}>
       <div className={styles.showcaseImage}></div>
-      <div className={`flex p-30 flex-justify-center flex-column`}>
+      <div className={`flex p-30 flex-justify-center flex-column ${styles.storiesTextSection}`}>
         <div>
           <h2 className="text-color--dark-1">
             Choose from beautiful backgrounds and share to the world what you are up to.
